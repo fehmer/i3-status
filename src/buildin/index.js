5abafb849ef439f62fa07d8a9d6abf8bb1c25935
Eric Dubé <eric.alex.dube@gmail.com>
2022-09-29 12:20:49 -0400

    Add `lanip` type (#2)
    * Add lanip type
    
    * Add documentation
    
    Co-authored-by: KernelDeimos <eric@centos1.rack1.local>

--- src/buildin/index.js
+++ src/buildin/index.js
@@ -6,6 +6,7 @@ import command from './command';
 import date from './date';
 import diskfree from './diskfree';
 import hostname from './hostname';
+import lanip from './lanip';
 import loadavg from './loadavg';
 import memory from './memory';
 import text from './text';
@@ -20,6 +21,7 @@ export default {
     date,
     diskfree,
     hostname,
+    lanip,
     loadavg,
     memory,
     text,
---
+++ src/buildin/lanip.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @module builtin/lanip */
+
+import { EventEmitter } from 'events';
+import { networkInterfaces } from 'os';
+
+export default class LanIP extends EventEmitter {
+    /**
+     * @param {Object} options - block configuration from config file
+     * @param {Object} output - block output for i3bar
+     */
+    constructor(options, output) {
+        super();
+        options = options || {};
+        this.output = output || {};
+
+        this.interface = options.interface;
+    }
+
+    /**
+     * update the block's output with the IP of the specified interface
+     */
+    update() {
+        const output = this.buildText();
+
+        output.full_text = output.text;
+        output.short_text = output.text;
+
+        this.output = output;
+        this.emit('updated', this, output);
+
+        return output;
+    }
+
+    buildText() {
+        const output = {};
+
+        const interfaces = networkInterfaces();
+        const networks = interfaces[this.interface];
+
+        if ( ! networks ) {
+            output.text = `interface not found: '${this.interface}'`;
+            return;
+        }
+
+        if ( networks.length < 1 ) {
+            output.text = `No IP`;
+            return;
+        }
+
+        output.text = networks[0].address;
+
+        return output;
+    }
+}
